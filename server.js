5411a3309df340b1c1c7e14392d85e5c13a5578a
Mustaqeem <37992013+mbangi66@users.noreply.github.com>
2019-08-21 14:32:24 +0530

    adding port

--- server.js
+++ server.js
@@ -27,15 +27,15 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => { req.send(db.users) })
+app.get('/', (req, res) => { req.send('its working!') })
 app.post('/signin', (req, res) =>{signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res, myPlaintextPassword, saltRounds) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3001, () => {
-	console.log('app is running on port 3001');
+app.listen(process.env.PORT || 3000, () => {
+	console.log('app is running on port ${process.env.PORT}');
 })
 
 
